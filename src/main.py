cfb56b7363477b215e188880dddf7b483090d2b6
Jack Hayley <66978120+Andethy@users.noreply.github.com>
2024-08-26 09:26:06 -0400

    Add 2 demos

--- src/main.py
+++ src/main.py
@@ -8,10 +8,11 @@ def main():
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    while True:
+    value = 'tmp'
+    while value:
         value = input("Update value: ")
         sock.sendto(value.encode(), (udp_ip, udp_port))
-        time.sleep(0.1)  # Adjust the update speed (0.1 seconds)
+
 
 
 if __name__ == '__main__':
