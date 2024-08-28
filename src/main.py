eaa5cbd416d8a086e935b68a3140d51769ff32fe
Jack Hayley <66978120+Andethy@users.noreply.github.com>
2024-08-28 10:10:49 -0400

    Clean main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
 import socket
 import tkinter as tk
 
-def send_value(value):
+def send_value(value: str) -> None:
     udp_ip = "127.0.0.1"
     udp_port = 54637  # Port number -> JUCE
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(value.encode(), (udp_ip, udp_port))
 
-def on_slider_change(value):
-    send_value(str(float(value) / 100))  # Send the slider value when it changes
+def on_slider_change(value: str) -> None:
+    send_value(str(float(value) / 100))  # Bound the value -> str
     print("Changed to " + value)
 
-def main():
+def main() -> None:
     root = tk.Tk()
     root.title("Slider to UDP")
     root.geometry("400x50")
