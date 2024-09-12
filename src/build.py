464266ca92c895d791d7eec74bcef6da3791ff99
jhayley3 <jhayley3@gatech.edu>
2024-09-11 22:47:42 -0400

    feat(build): build.py expanded to ease plugin installation

---
+++ src/build.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+path_to_build_au = "../plugin/Automator/Builds/MacOSX/build/Debug/Automator.component"
+path_to_plugin_au = '/Library/Audio/Plug-Ins/Components/Automator.component'
+
+path_to_build_vst3 = "../plugin/Automator/Builds/MacOSX/build/Debug/Automator.vst3"
+path_to_plugin_vst3 = '/Library/Audio/Plug-Ins/VST3/Automator.vst3'
+
+if __name__ == '__main__':
+    try:
+        if os.path.exists(path_to_plugin_au):
+            shutil.rmtree(path_to_plugin_au)
+        shutil.copytree(path_to_build_au, path_to_plugin_au)
+        print(f"Plugin copied to {path_to_plugin_au}")
+    except Exception as e:
+        print(f"OOPs: {e}")
+
+    try:
+        # if os.path.exists(path_to_plugin_vst3):
+        #     shutil.rm(path_to_plugin_vst3)
+        shutil.copy(path_to_build_vst3, path_to_plugin_vst3)
+        print(f"Plugin copied to {path_to_plugin_vst3}")
+    except Exception as e:
+        print(f"OOPs: {e}")
---
+++ src/osc.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from pythonosc import udp_client
+
+# OSC Configuration
+ip = "127.0.0.1"
+port = 54637
+
+# Function to handle slider value change and send it via OSC
+def send_value(value):
+    x = float(value)
+    client.send_message("/melody", x)  # Send the slider value to the OSC route
+    print("Sent", x)
+
+# Main application
+def main():
+    global client
+    client = udp_client.SimpleUDPClient(ip, port)
+
+    # Create the Tkinter window
+    root = tk.Tk()
+    root.title("OSC Slider")
+
+    # Create a label
+    label = tk.Label(root, text="Melody Slider")
+    label.pack()
+
+    # Create the slider widget
+    slider = tk.Scale(root, from_=0, to=1, orient="horizontal", resolution=0.01, command=send_value)
+    slider.pack()
+
+    # Start the Tkinter main loop
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
