1b02130a9bf4753a267d9f29032e26e7edfd950e
jhayley3 <jhayley@gatech.edu>
2025-01-28 02:12:48 -0500

    feat(fluency-m2): implement harmony (2 voice demo)

--- common/src/common/osc.py
+++ common/src/common/osc.py
@@ -13,20 +13,23 @@ class OSCManager:
 
         # Case A: Set ports specified in args
         for port in ports:
+            print(f'Setup client on port {port} (de)')
             self.ports[port] = udp_client.SimpleUDPClient(address, port)
 
         # Case B: Ascending from base e.g. 1000, 1001, 1002, ... x k
         for i in range(k):
+            print(f'Setup client on port {base + i} (bi)')
             self.ports[base + i] = udp_client.SimpleUDPClient(address, base + i)
 
         # Case C: custom port setup
         for port, client in kwargs.items():
+            print(f'Setup client on port {port} (dc)')
             # noinspection PyTypeChecker
             self.ports[port] = client
 
-    def __call__(self, *args, **kwargs):
-        for port, val in args:
-            self[port] = val
+    def __call__(self, port, addr, val, *args, **kwargs):
+        print(f'Sending msg on port {port} to {addr} set to {val}')
+        self[port].send_message(addr, float(val))
 
     def __getitem__(self, item):
         return self.ports[item]
@@ -34,3 +37,6 @@ class OSCManager:
     def __setitem__(self, key, value):
         self.ports[key].send_message(key, float(value))
 
+    def reconnect(self):
+        for port in self.ports:
+            self.ports[port] = udp_client.SimpleUDPClient(self.address, port)
--- fluency/m2-rhythm/src/main.py
+++ fluency/m2-rhythm/src/main.py
@@ -3,9 +3,10 @@ from melody import MelodyGenerator
 
 if __name__ == "__main__":
     simulation = True
-    xarm = utils.Robot("192.168.1.215", simulation)
+    xarm = utils.Robot("192.168.1.215", sim=simulation)
     if not simulation:
         xarm.setupBot()
     mg = MelodyGenerator(scale='major')
     # xarm.from_file('resources/demo.txt', 4)
-    xarm.from_file(mg.generate(length=1), 5)
+    # xarm.from_file(mg.generate(length=1), 5)
+    xarm.harmony(5)
